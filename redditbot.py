a25331d5d9b43de8b64356e083a9591f67c19d8c
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-03-18 21:46:35 +0330

    # just like that :) / Uncle Bamdad

--- redditbot.py
+++ redditbot.py
@@ -15,4 +15,46 @@ reddit.user.me()
 sub_reddit = reddit.subreddit("learnPython")
 for submission in sub_reddit.hot(limit=5):
     print(submission.title)
+    if not submission.stickied:
+        print(f"title: {submission.title}")
+        print(f"score: {submission.score}")
 
+
+# posting to reddit
+reddit.post( title= "blah blah",
+    selftext = "blaaaaaaahhhh")
+
+reddit.random_subreddit("random things")
+
+reddit.comment()
+reddit.delete()
+reddit.get()
+reddit.inbox.messages()
+reddit.inbox.all()
+reddit.live.info()
+
+ids = ["3rgnbke2rai6hen7ciytwcxadi", "sw7bubeycai6hey4ciytwamw3a", "t8jnufucss07"]
+for thread in reddit.live.info(ids):
+        print(thread.title)
+
+# can you see how much python is amazing? 
+        
+reddit.redditors.new()
+reddit.multireddit.create()
+
+reddit.request()
+
+reddit.submission()
+
+reddit.user.blocked()
+reddit.user.friends()
+reddit.user.contributor_subreddits()
+reddit.user.karma()
+reddit.user.moderator_subreddits()
+
+for subreddit in reddit.user.moderator_subreddits(limit=None):
+        print(str(subreddit))
+
+
+
+# just like that :) / Uncle Bamdad
