a5ff56e87181af524fadfe2ecc5a02904a06bd99
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-03-18 21:24:05 +0330

    fu

--- redditbot.py
+++ redditbot.py
@@ -1,4 +1,8 @@
 import praw  
 # praw stands for => python reddit API wrapper
-# like telegram apps, you need api key for reddit apps too
+# like telegram apps, you need keys for reddit apps too
 # go to reddit.com/prefs/apps  => from there create your app
+
+reddit = praw.Reddit(client_id="your c id",
+                     client_secret="your c s",
+                     user_agent="myBot/0.0.1")
