b7df9c43f16a1e7686e3e6689dcdf51f0d1287de
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-03-18 21:27:35 +0330

    lpb

--- redditbot.py
+++ redditbot.py
@@ -3,6 +3,14 @@ import praw
 # like telegram apps, you need keys for reddit apps too
 # go to reddit.com/prefs/apps  => from there create your app
 
+# build the instance
 reddit = praw.Reddit(client_id="your c id",
                      client_secret="your c s",
                      user_agent="myBot/0.0.1")
+
+# login to reddit
+reddit.user.me()
+
+# get data from reddit
+subreddit = reddit.subreddit("learnPython")
+
