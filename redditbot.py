ccdd6e6b6e63cd1d68b90e45c9d09e8e1a9a2b66
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-03-18 21:19:58 +0330

    api key desc

--- redditbot.py
+++ redditbot.py
@@ -1,1 +1,4 @@
-import praw  # praw stands for => python reddit API wrapper
+import praw  
+# praw stands for => python reddit API wrapper
+# like telegram apps, you need api key for reddit apps too
+# go to reddit.com/prefs/apps  => from there create your app
